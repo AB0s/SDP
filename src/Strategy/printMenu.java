849289978607f87d0d4a6b2c0b1a8a6827ccca31
AB0s <93539709+AB0s@users.noreply.github.com>
2022-11-26 23:45:39 +0600

    Update printMenu.java

--- src/Strategy/printMenu.java
+++ src/Strategy/printMenu.java
@@ -3,6 +3,6 @@ package Strategy;
 public class printMenu implements Strategy{
     @Override
     public void PrintingMenu() {
-        System.out.println("1.Show all vacancies \n2.Add Vacancy \n3.Delete Vacancy \n4.Subscribe to notifications");
+        System.out.println("1.Show all vacancies. \n2.Add Vacancy. \n3.Delete Vacancy. \n4.Subscribe to notifications.");
     }
 }
